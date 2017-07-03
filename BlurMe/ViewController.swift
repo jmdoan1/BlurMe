4f09e9ad5542e50f9240aad7ed0399dd2d347dda
Justin Doan <justindoan@justindoan.com>
2017-07-02 21:48:17 -0400

    Initial Commit

---
+++ BlurMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlurMe
+//
+//  Created by Justin Doan on 7/2/17.
+//  Copyright © 2017 Justin Doan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
